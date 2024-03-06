9b0f236f96893e21523050baf01ac9ceb74a72e5
lakshyaQ94 <147408557+lakshyaQ94@users.noreply.github.com>
2024-03-06 23:40:10 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ let searchbtn = document.getElementById("searchbtn");
 let inputData = document.getElementById("inputData");
 let searchType = document.getElementById("type");
 
-const getData = async (input) => {
-    let res = await fetch (`https://newsapi.org/v2/everything?q=${input}&apiKey=${key}`);
+const getData = async () => {
+    let res = await fetch (`https://newsapi.org/v2/everything?q=country&apiKey=${key}`);
     let jsonData = await res.json();
     console.log(jsonData.articles);
 
     cardData.innerHTML="";
-    jsonData.articles.forEach(function(article){
-        console.log(article);
+    jsonData.articles.forEach(function(articles){
+        console.log(articles);
 
     let divs = document.createElement("div");
     divs.classList.add("card");
     cardData.appendChild(divs);
 
     divs.innerHTML= `
-    <img src="${article.urlToImage}" alt="">
-    <h3>${article.title}</h3>
-    <p>${article.description}</p>
+    <img src="${articles.urlToImage}" alt="">
+    <h3>${articles.title}</h3>
+    <p>${articles.description}</p>
     `
 
     divs.addEventListener("click", function(){
-        window.open(article.url);
+        window.open(articles.url);
     })
     })
 
 }
 
 window.addEventListener("load", function(){
-    getData("India");
+    getData("world");
 });
 
 searchbtn.addEventListener("click", function() {
